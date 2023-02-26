c4f2a3f8337da4d151d68186802bdf9d3763f2b0
FlatHippo <47155251+FlatHippo@users.noreply.github.com>
2023-02-25 22:15:45 -0500

    shmoove SAVE DATA

---
+++ Data.java
@@ -0,0 +1,12 @@
+public class Data {
+    public int day;
+    public int job;
+    public double walletBalance;
+    public double bankBalance;
+    Data(int d, double wBal, double bBal, int j){
+        day = d;
+        walletBalance = wBal;
+        bankBalance = bBal;
+        job = j;
+    }
+}
